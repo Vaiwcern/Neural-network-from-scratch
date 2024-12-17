2091dcc0243b371ece447dbb5a16c0c7f2c66dc0
Nguyễn Phúc Tân <21120028@student.hcmus.edu.vn>
2024-12-17 22:18:03 +0700

    add class loader
    load dataset

--- gpu_ver1/src/main.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-#include <vector>
-#include "neural_network.h"
-#include "kernel.h"
-
-int main() {
-    
-    return 0;
-}
---
+++ gpu_ver1/src/main.cu
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main() {
+    string image_file_train = "data/train-images-idx3-ubyte";
+    string label_file_train = "data/train-labels-idx1-ubyte";
+    string image_file_test = "data/t10k-images-idx3-ubyte";
+    string label_file_test = "data/t10k-labels-idx1-ubyte";
+
+    
+
+    return 0;
+}
