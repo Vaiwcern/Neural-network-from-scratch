69240c4a7f232464a40104884ec06c44cb989009
Nguyễn Phúc Tân <21120028@student.hcmus.edu.vn>
2024-12-18 10:09:30 +0700

    x

--- gpu_ver1/src/utils.cu
+++ gpu_ver1/src/utils.cu
@@ -1,3 +1,6 @@
+#include <iostream>
+#include <vector>  // Đảm bảo bạn bao gồm thư viện vector
+
 // Hàm để chuẩn hóa dữ liệu hình ảnh từ unsigned char sang float
 void normalize_data(vector<unsigned char>& images, float* output, int num_samples, int image_size) {
     for (int i = 0; i < num_samples; ++i) {
