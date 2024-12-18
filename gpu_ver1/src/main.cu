df61949d927a81a176fa08ba5b01e368ed08ca82
Nguyễn Phúc Tân <21120028@student.hcmus.edu.vn>
2024-12-18 09:56:49 +0700

    x

--- gpu_ver1/src/main.cu
+++ gpu_ver1/src/main.cu
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <vector>
 #include "loader.h"
+#include "ANN.h"  // Bao gồm lớp ANN
 
 using namespace std;
 
@@ -15,28 +16,24 @@ int main() {
     Dataset train_data = load_data(image_file_train, label_file_train);
     Dataset test_data = load_data(image_file_test, label_file_test);
 
-    // In thông tin về dữ liệu huấn luyện
-    cout << "Training data:" << endl;
-    cout << "Number of samples: " << train_data.num_samples << endl;
-    cout << "Image size: " << train_data.image_size << " pixels" << endl;
+    // Khởi tạo ANN model
+    int input_size = 784;  // 28x28 pixels
+    int hidden_size = 128;  // Số nơ-ron trong lớp ẩn
+    int output_size = 10;   // 10 lớp đầu ra cho 10 nhãn (0-9)
+    float learning_rate = 0.01f;
 
-    // In thông tin về dữ liệu kiểm tra
-    cout << "\nTest data:" << endl;
-    cout << "Number of samples: " << test_data.num_samples << endl;
-    cout << "Image size: " << test_data.image_size << " pixels" << endl;
+    // Các tham số huấn luyện
+    int batch_size = 32;  // Kích thước của mỗi batch
+    int num_epochs = 10;  // Số epochs (vòng lặp huấn luyện)
 
-    // In nhãn của một số mẫu để kiểm tra
-    cout << "\nSample labels from training data:" << endl;
-    for (int i = 0; i < 10; ++i) {
-        cout << "Label for image " << i + 1 << ": " << (int)train_data.labels[i] << endl;
-    }
+    // Tạo đối tượng ANN và huấn luyện mô hình
+    ANN ann(input_size, hidden_size, output_size, learning_rate);
+    
+    // Chuyển dữ liệu huấn luyện vào ANN, với batch size và epoch
+    ann.train(train_data.images.data(), train_data.labels.data(), batch_size, num_epochs);
 
-    // In vài pixel đầu tiên của một số hình ảnh để kiểm tra
-    cout << "\nSample image pixels (first 10 pixels) from the first image in training data:" << endl;
-    for (int i = 0; i < 10; ++i) {
-        cout << (int)train_data.images[i] << " ";
-    }
-    cout << endl;
+    // Đánh giá mô hình trên bộ dữ liệu kiểm tra
+    ann.eval(test_data.images.data(), test_data.labels.data(), test_data.num_samples);
 
     return 0;
 }
