a0c7565fdcb53f11c368f39fd9b14a0841df9668
Nguyễn Phúc Tân <21120028@student.hcmus.edu.vn>
2024-12-18 11:02:41 +0700

    x

--- gpu_ver1/src/main.cu
+++ gpu_ver1/src/main.cu
@@ -33,24 +33,39 @@ int main() {
     float* test_labels = new float[test_data.num_samples * 10];  // One-hot encoding
     normalize_labels(test_data.labels, test_labels, test_data.num_samples);
 
-    // Khởi tạo ANN model
-    int input_size = 784;  // 28x28 pixels
-    int hidden_size = 128;  // Số nơ-ron trong lớp ẩn
-    int output_size = 10;   // 10 lớp đầu ra cho 10 nhãn (0-9)
-    float learning_rate = 0.01f;
-
-    // Các tham số huấn luyện
-    int batch_size = 32;  // Kích thước của mỗi batch
-    int num_epochs = 10;  // Số epochs (vòng lặp huấn luyện)
-
-    // Tạo đối tượng ANN và huấn luyện mô hình
-    ANN ann(input_size, hidden_size, output_size, learning_rate);
+    for (int i = 0; i < 28 * 28; ++i) {
+        if (i%28) 
+            cout << "\n";
+        cout << train_images[i] << " ";
+    }
+    cout << "\n";
     
-    // Huấn luyện mô hình với dữ liệu huấn luyện
-    ann.train(train_images, train_labels, train_data.num_samples, batch_size, num_epochs);
+    for (int i = 0; i < 10; ++i) {
+        cout << train_labels[i] << " ";
+    }
+    cout << "\n";
 
-    // Đánh giá mô hình trên bộ dữ liệu kiểm tra
-    ann.eval(test_images, test_labels, test_data.num_samples);
+
+    // TRAIN AND TEST MODEL
+
+    // // Khởi tạo ANN model
+    // int input_size = 784;  // 28x28 pixels
+    // int hidden_size = 128;  // Số nơ-ron trong lớp ẩn
+    // int output_size = 10;   // 10 lớp đầu ra cho 10 nhãn (0-9)
+    // float learning_rate = 0.01f;
+
+    // // Các tham số huấn luyện
+    // int batch_size = 32;  // Kích thước của mỗi batch
+    // int num_epochs = 10;  // Số epochs (vòng lặp huấn luyện)
+
+    // // Tạo đối tượng ANN và huấn luyện mô hình
+    // ANN ann(input_size, hidden_size, output_size, learning_rate);
+    
+    // // Huấn luyện mô hình với dữ liệu huấn luyện
+    // ann.train(train_images, train_labels, train_data.num_samples, batch_size, num_epochs);
+
+    // // Đánh giá mô hình trên bộ dữ liệu kiểm tra
+    // ann.eval(test_images, test_labels, test_data.num_samples);
 
     // Giải phóng bộ nhớ
     delete[] train_images;
