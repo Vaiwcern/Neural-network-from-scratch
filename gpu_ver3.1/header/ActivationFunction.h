6ed70c1e014ff8be29fc1bee076882b44a76dddb
Nguyễn Phúc Tân <21120028@student.hcmus.edu.vn>
2024-12-19 22:02:47 +0700

    x

--- gpu_ver3.1/header/ActivationFunction.h
+++ gpu_ver3.1/header/ActivationFunction.h
@@ -2,20 +2,21 @@
 #define ACTIVATION_FUNCTION_H
 
 #include <vector>
+#include <cuda_fp16.h>  // Thêm thư viện để làm việc với float16
 
 // Lớp cơ sở ActivationFunction
 class ActivationFunction {
 public:
-    virtual void activate(float* input, float* output, int size) const = 0;  // Hàm kích hoạt cho toàn bộ vector
-    virtual void derivative(float* output, float* d_output, int size) const = 0; // Đạo hàm hàm kích hoạt
+    virtual void activate(half* input, half* output, int size) const = 0;  // Hàm kích hoạt cho toàn bộ vector
+    virtual void derivative(half* output, half* d_output, int size) const = 0; // Đạo hàm hàm kích hoạt
     virtual ~ActivationFunction() {}
 };
 
 // Lớp con ReLU
 class ReLU : public ActivationFunction {
 public:
-    void activate(float* input, float* output, int size) const override;  // Kích hoạt ReLU cho toàn bộ vector
-    void derivative(float* output, float* d_output, int size) const override; // Đạo hàm ReLU
+    void activate(half* input, half* output, int size) const override;  // Kích hoạt ReLU cho toàn bộ vector
+    void derivative(half* output, half* d_output, int size) const override; // Đạo hàm ReLU
 };
 
 // Lớp con Softmax
@@ -23,8 +24,8 @@ public:
 // nên với Softmax layer cuối (dùng CrossEntropy), ta có thể bỏ qua việc tính đạo hàm activation vì đã gộp vào loss.
 class Softmax : public ActivationFunction {
 public:
-    void activate(float* input, float* output, int size) const override;  // Softmax cho toàn bộ vector
-    void derivative(float* output, float* d_output, int size) const override; // Không thực sự cần cho output layer (CE loss)
+    void activate(half* input, half* output, int size) const override;  // Softmax cho toàn bộ vector
+    void derivative(half* output, half* d_output, int size) const override; // Không thực sự cần cho output layer (CE loss)
 };
 
 #endif
