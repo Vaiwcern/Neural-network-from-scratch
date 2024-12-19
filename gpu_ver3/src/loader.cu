20a5464ac0d136a637f3b90184b0d70023224385
Nguyễn Phúc Tân <21120028@student.hcmus.edu.vn>
2024-12-19 19:57:44 +0700

    x

--- gpu_ver3/src/loader.cu
+++ gpu_ver3/src/loader.cu
@@ -6,7 +6,7 @@ static int32_t readInt(ifstream &f)
 {
     int32_t val = 0;
     f.read((char *)&val, 4);
-    // convert from big-endian to little-endian
+    // Convert from big-endian to little-endian
     val = ((val & 0x000000FF) << 24) |
           ((val & 0x0000FF00) << 8) |
           ((val & 0x00FF0000) >> 8) |
@@ -25,7 +25,7 @@ vector<half> readIDXFile(const string &filename)
     int magic_number = readInt(file);
     int num_items = readInt(file);
 
-    // Nếu là ảnh
+    // If it's image data
     if (magic_number == 0x00000803)
     {
         int rows = readInt(file);
@@ -33,19 +33,19 @@ vector<half> readIDXFile(const string &filename)
         vector<half> data(num_items * rows * cols);
         vector<unsigned char> raw_data(num_items * rows * cols);
 
-        // Đọc dữ liệu vào mảng raw_data
+        // Read raw data into raw_data
         file.read((char *)raw_data.data(), raw_data.size());
 
-        // Chuyển dữ liệu từ unsigned char sang half
+        // Convert raw data from unsigned char to half
         for (size_t i = 0; i < raw_data.size(); ++i) {
-            data[i] = __float2half((float)raw_data[i] / 255.0f);  // Chuyển từ unsigned char (0-255) thành half (0.0f - 1.0f)
+            data[i] = __float2half((float)raw_data[i] / 255.0f);  // Convert from unsigned char (0-255) to half (0.0f - 1.0f)
         }
 
         return data;
     }
     else if (magic_number == 0x00000801)
     {
-        // labels
+        // Labels data
         vector<unsigned char> data(num_items);
         file.read((char *)data.data(), data.size());
         return data;
@@ -58,9 +58,9 @@ vector<half> readIDXFile(const string &filename)
 Dataset load_data(const string &image_file_train, const string &label_file_train)
 {
     Dataset ds;
-    ds.images = readIDXFile(image_file_train);  // ảnh sử dụng kiểu half
-    ds.labels = readIDXFile(label_file_train);  // nhãn vẫn giữ nguyên kiểu unsigned char
+    ds.images = readIDXFile(image_file_train);  // Image data as half
+    ds.labels = readIDXFile(label_file_train);  // Labels remain as unsigned char
     ds.num_samples = (int)ds.labels.size();
-    ds.image_size = 28 * 28;  // Cấu trúc ảnh có kích thước 28x28
+    ds.image_size = 28 * 28;  // Image size (28x28)
     return ds;
 }
