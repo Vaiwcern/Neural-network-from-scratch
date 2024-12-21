cc50a695def9eb22a30f8d932b762d80472d0893
Nguyễn Phúc Tân <21120028@student.hcmus.edu.vn>
2024-12-21 16:34:02 +0700

    x

--- gpu_ver3/src/DenseLayer.cu
+++ gpu_ver3/src/DenseLayer.cu
@@ -21,13 +21,15 @@ DenseLayer::DenseLayer(int input_size, int output_size, ActivationFunction *acti
     std::mt19937 gen(rd());
     std::normal_distribution<float> dist(0.0f, sqrtf(2.0f / input_size));
 
-    for (int i = 0; i < output_size; ++i)
-    {
-        for (int j = 0; j < input_size; ++j)
+    for (int k = 0; k < input_size; ++k) {
+        for (int i = 0; i < output_size; ++i)
         {
-            weights[i * input_size + j] = dist(gen);
+            for (int j = 0; j < input_size; ++j)
+            {
+                weights[i * input_size + j] = dist(gen);
+            }
+            biases[i] = 0.0f;
         }
-        biases[i] = 0.0f;
     }
 
     // Allocate GPU memory once
